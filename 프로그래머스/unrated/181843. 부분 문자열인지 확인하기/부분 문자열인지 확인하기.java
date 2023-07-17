7f087a4d2a84aa1f25724d45f8746d4605334ce2
김다운 <128446489+kimdaun98@users.noreply.github.com>
2023-07-17 11:09:27 +0900

    [unrated] Title: 부분 문자열인지 확인하기, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String my_string, String target) {
+        int answer = 0;
+        answer = my_string.indexOf(target);
+        if(answer!= -1){
+            return answer = 1;
+        }else{
+            return answer = 0;
+        }
+    }
+}
